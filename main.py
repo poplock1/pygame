5af7c1eabb81af2f5eea4b24526785e48d8110e8
Krystian Plich <krypli94@gmail.com>
2020-05-29 15:40:30 +0200

    OOP optimalization

--- main.py
+++ main.py
@@ -1,72 +1,64 @@
 import pygame
+import random
+import settings as stg
 from player import player
 from blocks import block
 
-display_width = 800
-display_heigh = 700
-FPS = 33
 
-block_img = pygame.image.load("grass.png")
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.game_display = pygame.display.set_mode((stg.display_width, stg.display_heigh))
+        pygame.display.set_caption("Platform")
+        self.clock = pygame.time.Clock()
+        self.running = True
 
-pygame.init()
+    def new_game(self):
+        self.run()
 
-game_display = pygame.display.set_mode((display_width, display_heigh))
-pygame.display.set_caption("Platform")
-clock = pygame.time.Clock()
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.clock.tick(stg.FPS)
+            self.events()
+            self.update(player)
+            self.draw(player, blocks)
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+
+        self.keys = pygame.key.get_pressed()
+
+        if self.keys[pygame.K_ESCAPE] and self.playing:
+            self.playing = False
+
+    def update(self, player):
+        player.update()
+
+    def draw(self, player, blocks):
+        self.game_display.fill((150, 150, 140))
+        for element in blocks:
+            element.draw(self.game_display)
+        player.draw(self.game_display)
+        pygame.display.flip()
+
+    def start_screen(self):
+        pass
+
+    def game_over_screen(self):
+        pass
 
 
-def draw():
-    game_display.fill((150, 150, 140))
-    for element in blocks:
-        element.draw(game_display)
-    player.draw(game_display)
-    pygame.display.flip()
-
-
-player = player(400, 600)
-blocks = [block(200, 500, block_img), block(300, 500, block_img)]
-run = True
-
-while run:
-    # Process input (events)
-    clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-
-    keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_ESCAPE]:
-        run = False
-    # Update
-    if keys[pygame.K_LEFT] and player.x > (player.rect.size[0]/2):
-        player.x -= player.vel
-        player.right = False
-        player.left = True
-    elif keys[pygame.K_RIGHT] and player.x <= (display_width - 3*(player.rect.size[0]/2)):
-        player.x += player.vel
-        player.right = True
-        player.left = False
-    else:
-        player.right = False
-        player.left = False
-        player.walkCount = 0
-
-    if keys[pygame.K_SPACE]:
-        player.isJumping = True
-
-    if player.isJumping:
-        neg = 1
-        if player.jumpCount >= -11:
-            if player.jumpCount < 0:
-                neg = -1
-            player.y -= (player.jumpCount ** 2) * 0.5 * neg
-            player.jumpCount -= 1
-        else:
-            player.isJumping = False
-            player.jumpCount = 11
-
-    # Draw
-    draw()
+game = Game()
+game.start_screen()
+while game.running:
+    player = player(400, 600)
+    blocks = [block(200, 500, stg.block_img), block(300, 500, stg.block_img)]
+    game.new_game()
+    game.game_over_screen()
 
 pygame.quit()
