7ed3ad72588ebe28b0f76c6498e34e25fa476f22
Krystian Plich <krypli94@gmail.com>
2020-05-29 18:30:26 +0200

    Add player movement settings

--- settings.py
+++ settings.py
@@ -1,7 +1,10 @@
 import pygame
-
+# Game options
 display_width = 800
 display_heigh = 700
-FPS = 33
-
+FPS = 60
+# Player properties
+player_acc = 1
+player_friction = -0.13
+# Block images
 block_img = pygame.image.load("grass.png")
