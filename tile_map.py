57d558b857416cbf90ac31274b8725ea1de1fa35
Krystian Plich <krypli94@gmail.com>
2020-06-01 21:14:09 +0200

    Add map maker

---
+++ tile_map.py
@@ -0,0 +1,25 @@
+import pytmx
+import pygame
+
+
+class TiledMap:
+    def __init__(self, filename):
+        tm = pytmx.load_pygame(filename, pixelaplha=True)
+        self.width = tm.width * tm.tilewidth
+        self.height = tm.height * tm.tileheight
+        self.tmxdata = tm
+
+    def render(self, surface):
+        ti = self.tmxdata.get_tile_image_by_gid
+        for layer in self.tmxdata.visible_layers:
+            if isinstance(layer, pytmx.TiledTileLayer):
+                for x, y, gid in layer:
+                    tile = ti(gid)
+                    if tile:
+                        surface.blit(tile, (x * self.tmxdata.tilewidth,
+                                            y * self.tmxdata.tileheight))
+
+    def make_map(self):
+        surface = pygame.Surface((self.width, self.height))
+        self.render(surface)
+        return surface
