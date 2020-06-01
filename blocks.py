3f53de01803721e460c1b8ab43cb5bf18253deb2
Krystian Plich <krypli94@gmail.com>
2020-06-01 21:12:59 +0200

    Update block to new map loader

--- blocks.py
+++ blocks.py
@@ -6,15 +6,14 @@ block_img = pygame.image.load("grass.png")
 
 class block(pygame.sprite.Sprite):
 
-    def __init__(self, x, y, block_img):
+    def __init__(self, x, y, width, height):
         pygame.sprite.Sprite.__init__(self)
-        self.x = x
-        self.y = y
         self.image = block_img
-        self.rect = self.image.get_rect(topleft=(self.x, self.y))
-        self.x_range = x + 64
+        self.rect = pygame.Rect(x, y, width, height)
+        self.rect.x = x
+        self.rect.y = y
 
-    def draw(self, game_display):
-        game_display.blit(block_img, (self.x, self.y))
-        self.rect = self.image.get_rect(topleft=(self.x, self.y))
-        pygame.draw.rect(game_display, (0, 255, 0), self.rect, 2)
+    # def draw(self, game_display):
+    #     game_display.blit(block_img, (self.x, self.y))
+    #     self.rect = self.image.get_rect(topleft=(self.x, self.y))
+    #     pygame.draw.rect(game_display, (0, 255, 0), self.rect, 2)
