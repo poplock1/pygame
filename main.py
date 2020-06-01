a868f3b1dbe4aa0bad130fa2cdaaae76cc7b5317
Krystian Plich <krypli94@gmail.com>
2020-06-01 21:11:23 +0200

    Add map loading function

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
 import pygame
-import random
+from os import path
 import settings as stg
-from player import player
+from tile_map import TiledMap
+from player import Player
 from blocks import block
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.game_display = pygame.display.set_mode((stg.display_width, stg.display_heigh))
+        self.game_display = pygame.display.set_mode(
+            (stg.display_width, stg.display_heigh))
         pygame.display.set_caption("Platform")
         self.game_display_rect = self.game_display.get_rect()
         self.view = self.game_display.get_rect()
-        self.level = pygame.Surface((stg.level_width, stg.level_heigh)).convert()
-        self.level_rect = self.level.get_rect()
         self.clock = pygame.time.Clock()
         self.running = True
+        self.load_data()
+
+    def load_data(self):
+        game_folder = path.dirname(__file__)
+        map_folder = path.join(game_folder, 'Maps')
+        self.map = TiledMap(path.join(map_folder, 'MAP1.tmx'))
+        self.map_img = self.map.make_map()
+        self.map_rect = self.map_img.get_rect()
 
     def new_game(self):
         self.all_sprites = pygame.sprite.Group()
-        self.player = player(self)
-        self.all_sprites.add(self.player)
         self.blocks = pygame.sprite.Group()
-        b1 = block(0, 680, 800, 20, stg.block_img)
-        b2 = block(20, 480, 60, 20, stg.block_img)
-        self.all_sprites.add(b1, b2)
-        self.blocks.add(b1, b2)
-        self.all_sprites.add(self.blocks)
+        for tile_object in self.map.tmxdata.objects:
+            if tile_object.name == "Player":
+                self.player = Player(
+                    tile_object.x, tile_object.y, tile_object.width, tile_object.height, self)
+                self.all_sprites.add(self.player)
+            if tile_object.name == "Ground":
+                self.block = block(tile_object.x, tile_object.y,
+                                   tile_object.width, tile_object.height)
+                self.all_sprites.add(self.block)
+                self.blocks.add(self.block)
         self.run()
- 
+
     def run(self):
         self.playing = True
         while self.playing:
@@ -56,21 +67,22 @@ class Game:
     def update(self):
         self.all_sprites.update()
         if self.player.vel.y > 0:
-            collision = pygame.sprite.spritecollide(self.player, self.blocks, False)
+            collision = pygame.sprite.spritecollideany(
+                self.player, self.blocks, False)
             if collision:
-                self.player.pos.y = collision[0].rect.top + 1
                 self.player.vel.y = 0
+                self.player.pos[1] = collision.rect.top + 1
         self.update_view()
 
     def draw(self):
-        self.level.fill((150, 150, 140))
-        self.all_sprites.draw(self.level)
-        self.game_display.blit(self.level, (0,0), self.view)
+        self.map_img.blit(
+            self.player.image, (self.player.pos[0], self.player.pos[1] - self.player.height))
+        self.game_display.blit(self.map_img, (0, 0), self.view)
         pygame.display.flip()
 
     def update_view(self):
         self.view.center = self.player.rect.center
-        self.view.clamp_ip(self.level_rect)
+        self.view.clamp_ip(self.map_rect)
 
     def start_screen(self):
         pass
